cb294096b694caa2eeacd4f1acf7477656a5dd67
mchow <mchow@Melanies-MacBook-Pro.local>
2018-09-02 09:57:35 -0400

    updated and node modules replaced

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  start:'https://safest-route-1535825298089.appspot.com/',
   apiKey:'AIzaSyCAOHpGqvNo0M1eRd58Hb2GGJLifR60a3A'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  start:'http://localhost:8000/',
+  start:'https://safest-route-1535825298089.appspot.com/',
   apiKey:'AIzaSyCAOHpGqvNo0M1eRd58Hb2GGJLifR60a3A'
 };
 
